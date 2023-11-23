484f52581d7a8ff62c96af7cb522297dee7fff19
= <=>
2023-11-23 13:14:41 +0700

    Fix SerialNumber

--- src/SerialNumber.java
+++ src/SerialNumber.java
@@ -1,6 +1,6 @@
 public class SerialNumber {
     String serialNumber = "FEDCBA9876543210";
-    public void getSerialNumber(String serialNumber) {
+    public void setSerialNumber(String serialNumber) {
         this.serialNumber = serialNumber;
     }
 }
